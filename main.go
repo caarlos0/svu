c120ed988717f3d32d4a9c63d5e61f21ca2842c0
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-12-13 14:54:28 -0200

    feat: first

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/masterminds/semver"
+
+	"github.com/alecthomas/kingpin"
+)
+
+var (
+	version = "dev"
+	app     = kingpin.New("svu", "semantic version util")
+	repo    = app.Flag("path", "git repository path").
+		Default(".").
+		Short('p').
+		ExistingDir()
+	major = app.Command("major", "new major version")
+	minor = app.Command("minor", "new minor version")
+	patch = app.Command("patch", "new patch version")
+)
+
+func main() {
+	app.Author("Carlos Alexandro Becker <caarlos0@gmail.com>")
+	app.Version("svu version " + version)
+	app.VersionFlag.Short('v')
+	app.HelpFlag.Short('h')
+	var cmd = kingpin.MustParse(app.Parse(os.Args[1:]))
+
+	tag, err := tagFromRepo(*repo)
+	app.FatalIfError(err, "failed to get current tag for repo %s", *repo)
+	current, err := semver.NewVersion(tag)
+	app.FatalIfError(err, "version %s is not semantic", tag)
+	var prefix string
+	if strings.HasPrefix(tag, "v") {
+		prefix = "v"
+	}
+
+	var next semver.Version
+	switch cmd {
+	case major.FullCommand():
+		next = current.IncMajor()
+	case minor.FullCommand():
+		next = current.IncMinor()
+	case patch.FullCommand():
+		next = current.IncPatch()
+	}
+	fmt.Printf("%s%s\n", prefix, next.String())
+}
+
+func tagFromRepo(path string) (string, error) {
+	cmd := exec.Command("git", "describe", "--tags", "--abbrev=0")
+	cmd.Dir = path
+	bts, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	return strings.Split(string(bts), "\n")[0], nil
+}
