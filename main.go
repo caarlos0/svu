3885b0a1a93f9be516a840d11c85108470e914e1
Dominik Lessel <dominik@lessel.xyz>
2020-11-10 08:58:04 +0100

    feat: add only core version flag

--- main.go
+++ main.go
@@ -12,13 +12,14 @@ import (
 )
 
 var (
-	version    = "dev"
-	app        = kingpin.New("svu", "semantic version util")
-	nextCmd    = app.Command("next", "prints the next version based on the git log").Alias("n").Default()
-	majorCmd   = app.Command("major", "new major version")
-	minorCmd   = app.Command("minor", "new minor version").Alias("m")
-	patchCmd   = app.Command("patch", "new patch version").Alias("p")
-	currentCmd = app.Command("current", "prints current version").Alias("c")
+	version         = "dev"
+	app             = kingpin.New("svu", "semantic version util")
+	nextCmd         = app.Command("next", "prints the next version based on the git log").Alias("n").Default()
+	majorCmd        = app.Command("major", "new major version")
+	minorCmd        = app.Command("minor", "new minor version").Alias("m")
+	patchCmd        = app.Command("patch", "new patch version").Alias("p")
+	currentCmd      = app.Command("current", "prints current version").Alias("c")
+	onlyCoreVersion = app.Flag("only-core-version", "discards pre-release and build metadata").Bool()
 )
 
 func main() {
@@ -33,6 +34,11 @@ func main() {
 
 	current, err := semver.NewVersion(tag)
 	app.FatalIfError(err, "version %s is not semantic", tag)
+
+	if *onlyCoreVersion {
+		current.SetPrerelease("")
+		current.SetMetadata("")
+	}
 
 	var prefix string
 	if strings.HasPrefix(tag, "v") {
