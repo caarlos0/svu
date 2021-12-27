f3b49ebac890cc3642d4c0531c27e6d212bbd6ab
Carlos A Becker <caarlos0@gmail.com>
2021-12-27 15:14:07 -0300

    test: improve test
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- internal/svu/svu_test.go
+++ internal/svu/svu_test.go
@@ -74,13 +74,17 @@ func TestIsPatch(t *testing.T) {
 func TestFindNext(t *testing.T) {
 	version1 := semver.MustParse("v1.2.3")
 	version2 := semver.MustParse("v2.4.12")
+	version3 := semver.MustParse("v3.4.5-beta34+ads")
 	for expected, next := range map[string]semver.Version{
 		"v1.2.3": FindNext(version1, false, "chore: should do nothing"),
 		"v1.2.4": FindNext(version1, true, "chore: is forcing patch, so should inc patch"),
 		"v1.3.0": FindNext(version1, false, "feat: inc major"),
 		"v2.0.0": FindNext(version1, true, "chore!: hashbang incs major"),
 		"v3.0.0": FindNext(version2, false, "feat: something\nBREAKING CHANGE: increases major"),
+		"v3.5.0": FindNext(version3, false, "feat: inc major"),
 	} {
-		is.New(t).True(semver.MustParse(expected).Equal(&next)) // expected and next version should match
+		t.Run(expected, func(t *testing.T) {
+			is.New(t).True(semver.MustParse(expected).Equal(&next)) // expected and next version should match
+		})
 	}
 }
