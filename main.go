29864d4c2461a0c4d63b92efb01031725b42e495
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-02-05 23:35:01 -0300

    fix: oops

--- main.go
+++ main.go
@@ -13,6 +13,15 @@ import (
 func main() {
 	var opts svu.Options
 
+	runFunc := func(cmd *cobra.Command, _ []string) error {
+		version, err := svu.Version(opts)
+		if err != nil {
+			return err
+		}
+		cmd.Println(version)
+		return nil
+	}
+
 	root := &cobra.Command{
 		Use:          "svu",
 		Short:        "Semantic Version Util",
@@ -33,48 +42,67 @@ func main() {
 		Use:     "prerelease",
 		Aliases: []string{"pr"},
 		Short:   "Increases the build portion of the prerelease",
-		RunE:    runE(opts, svu.PreReleaseCmd),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			opts.Cmd = svu.PreReleaseCmd
+			return runFunc(cmd, args)
+		},
 	}
 	next := &cobra.Command{
 		Use:     "next",
 		Aliases: []string{"n"},
 		Short:   "Next version based on git history",
-		RunE:    runE(opts, svu.NextCmd),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			opts.Cmd = svu.NextCmd
+			return runFunc(cmd, args)
+		},
 	}
 	major := &cobra.Command{
 		Use:   "major",
 		Short: "New major release",
-		RunE:  runE(opts, svu.MajorCmd),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			opts.Cmd = svu.MajorCmd
+			return runFunc(cmd, args)
+		},
 	}
 	minor := &cobra.Command{
 		Use:     "minor",
 		Short:   "New minor release",
 		Aliases: []string{"m"},
-		RunE:    runE(opts, svu.MinorCmd),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			opts.Cmd = svu.MinorCmd
+			return runFunc(cmd, args)
+		},
 	}
 	patch := &cobra.Command{
 		Use:     "patch",
 		Short:   "New patch release",
 		Aliases: []string{"p"},
-		RunE:    runE(opts, svu.PatchCmd),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			opts.Cmd = svu.PatchCmd
+			return runFunc(cmd, args)
+		},
 	}
 	current := &cobra.Command{
 		Use:     "current",
 		Short:   "Current version",
 		Aliases: []string{"c"},
-		RunE:    runE(opts, svu.CurrentCmd),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			opts.Cmd = svu.CurrentCmd
+			return runFunc(cmd, args)
+		},
 	}
 
-	root.PersistentFlags().StringVar(&opts.PreRelease, "pre-release", "", "adds a pre-release suffix to the version, without the semver mandatory dash prefix")
 	root.PersistentFlags().StringVar(&opts.Pattern, "pattern", "", "limits calculations to be based on tags matching the given pattern")
+
 	root.PersistentFlags().StringVar(&opts.Prefix, "prefix", "v", "sets a custom prefix")
 	root.PersistentFlags().BoolVar(&opts.StripPrefix, "strip-prefix", false, "strips any prefixes the tag might have")
-	root.PersistentFlags().StringVar(&opts.Build, "build", "", "adds a build suffix to the version, without the semver mandatory plug prefix")
 	root.PersistentFlags().StringVar(&opts.Directory, "directory", ".", "limit git operations to a directory")
 	root.PersistentFlags().StringVar(&opts.TagMode, "tag-mode", "current-branch", "determines if latest tag of the current or all branches will be used (curent-branch, all-branches)")
 
-	next.PersistentFlags().BoolVar(&opts.ForcePatchIncrement, "force-patch-increment", false, "forces a patch version increment regardless of the commit message content")
-	next.PersistentFlags().BoolVar(&opts.PreventMajorIncrementOnV0, "no-increment-v0", false, "prevent major version increments when its still v0")
+	next.Flags().StringVar(&opts.Build, "build", "", "adds a build suffix to the version, without the semver mandatory plug prefix")
+	next.Flags().StringVar(&opts.PreRelease, "pre-release", "", "adds a pre-release suffix to the version, without the semver mandatory dash prefix")
+	next.Flags().BoolVar(&opts.ForcePatchIncrement, "force-patch-increment", false, "forces a patch version increment regardless of the commit message content")
+	next.Flags().BoolVar(&opts.PreventMajorIncrementOnV0, "no-increment-v0", false, "prevent major version increments when its still v0")
 
 	root.AddCommand(
 		next,
@@ -87,18 +115,6 @@ func main() {
 
 	if err := root.Execute(); err != nil {
 		os.Exit(1)
-	}
-}
-
-func runE(opts svu.Options, action string) func(*cobra.Command, []string) error {
-	return func(cmd *cobra.Command, args []string) error {
-		opts.Cmd = action
-		version, err := svu.Version(opts)
-		if err != nil {
-			return err
-		}
-		cmd.Println(version)
-		return nil
 	}
 }
 
