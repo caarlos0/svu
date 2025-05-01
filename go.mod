b6a014b0caf7667f5d2a2660b980291100ffcdba
Artur Faizullin <artur.faizullin@aimms.com>
2025-05-01 23:02:05 +0200

    Use go-git impelementation

--- go.mod
+++ go.mod
@@ -1,11 +1,15 @@
 module github.com/caarlos0/svu/v3
 
-go 1.22
+go 1.23.0
+
+toolchain go1.23.7
 
 require (
 	github.com/Masterminds/semver/v3 v3.3.1
 	github.com/caarlos0/go-version v0.2.0
+	github.com/go-git/go-git/v5 v5.16.0
 	github.com/gobwas/glob v0.2.3
+	github.com/hashicorp/go-version v1.7.0
 	github.com/spf13/cobra v1.9.1
 	github.com/spf13/pflag v1.0.6
 	github.com/spf13/viper v1.20.1
@@ -13,20 +17,38 @@ require (
 )
 
 require (
+	dario.cat/mergo v1.0.0 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/ProtonMail/go-crypto v1.1.6 // indirect
+	github.com/cloudflare/circl v1.6.1 // indirect
+	github.com/cyphar/filepath-securejoin v0.4.1 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/emirpasic/gods v1.18.1 // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
+	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
+	github.com/go-git/go-billy/v5 v5.6.2 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/kevinburke/ssh_config v1.2.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/pjbgf/sha1cd v0.3.2 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/sagikazarmark/locafero v0.7.0 // indirect
+	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
+	github.com/skeema/knownhosts v1.3.1 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.12.0 // indirect
 	github.com/spf13/cast v1.7.1 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/xanzy/ssh-agent v0.3.3 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/net v0.39.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/git/git.go
+++ internal/git/git.go
@@ -1,12 +1,16 @@
 package git
 
 import (
-	"errors"
 	"fmt"
-	"os/exec"
+	"io"
+	"sort"
 	"strings"
 
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/gobwas/glob"
+	"github.com/hashicorp/go-version"
 )
 
 // Commit is a commit with a hash, title (first line of the message), and body
@@ -26,33 +30,112 @@ const (
 	TagModeCurrent = "current"
 )
 
-// copied from goreleaser
-
 // IsRepo returns true if current folder is a git repository
 func IsRepo() bool {
-	out, err := run("rev-parse", "--is-inside-work-tree")
-	return err == nil && strings.TrimSpace(out) == "true"
+	_, err := git.PlainOpen(".")
+	return err == nil
 }
 
 func Root() string {
-	out, _ := run("rev-parse", "--show-toplevel")
-	return strings.TrimSpace(out)
+	repo, err := git.PlainOpen(".")
+	if err != nil {
+		return ""
+	}
+
+	wt, err := repo.Worktree()
+	if err != nil {
+		return ""
+	}
+
+	return wt.Filesystem.Root()
 }
 
-func getAllTags(args ...string) ([]string, error) {
-	tags, err := run(append([]string{"-c", "versionsort.suffix=-", "tag", "--sort=-version:refname"}, args...)...)
+type versionSorter struct {
+	tags     []string
+	versions []*version.Version
+}
+
+func (v *versionSorter) Len() int {
+	return len(v.tags)
+}
+
+func (v *versionSorter) Swap(i, j int) {
+	v.tags[i], v.tags[j] = v.tags[j], v.tags[i]
+	v.versions[i], v.versions[j] = v.versions[j], v.versions[i]
+}
+
+func (v *versionSorter) Less(i, j int) bool {
+	return v.versions[i].LessThan(v.versions[j])
+}
+
+func getAllTags(tagMode string) ([]string, error) {
+	repo, err := git.PlainOpen(".")
 	if err != nil {
 		return nil, err
 	}
-	return strings.Split(tags, "\n"), nil
+
+	tagRefs, err := repo.Tags()
+	if err != nil {
+		return nil, err
+	}
+
+	var tags []string
+	var versions []*version.Version
+
+	err = tagRefs.ForEach(func(tagRef *plumbing.Reference) error {
+		tagName := strings.TrimPrefix(tagRef.Name().String(), "refs/tags/")
+
+		if tagMode == TagModeCurrent {
+			// Check if tag is reachable from HEAD
+			head, err := repo.Head()
+			if err != nil {
+				return err
+			}
+
+			commit, err := repo.CommitObject(head.Hash())
+			if err != nil {
+				return err
+			}
+
+			tagCommit, err := repo.CommitObject(tagRef.Hash())
+			if err != nil {
+				// Skip if it's not a commit (e.g., annotated tag)
+				return nil
+			}
+
+			isAncestor, err := commit.IsAncestor(tagCommit)
+			if err != nil || !isAncestor {
+				return nil
+			}
+		}
+
+		// Try to parse as version
+		v, err := version.NewVersion(tagName)
+		if err != nil {
+			// If not a version, we'll sort these alphabetically after versions
+			v, _ = version.NewVersion("0.0.0")
+		}
+
+		tags = append(tags, tagName)
+		versions = append(versions, v)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Sort tags by version (descending)
+	sorter := &versionSorter{
+		tags:     tags,
+		versions: versions,
+	}
+	sort.Sort(sort.Reverse(sorter))
+
+	return sorter.tags, nil
 }
 
 func DescribeTag(tagMode string, pattern string) (string, error) {
-	args := []string{}
-	if tagMode == TagModeCurrent {
-		args = []string{"--merged"}
-	}
-	tags, err := getAllTags(args...)
+	tags, err := getAllTags(tagMode)
 	if err != nil {
 		return "", err
 	}
@@ -78,55 +161,119 @@ func DescribeTag(tagMode string, pattern string) (string, error) {
 
 func Changelog(tag string, dirs []string) ([]Commit, error) {
 	if tag == "" {
-		return gitLog(dirs, "HEAD")
+		return gitLog(dirs, plumbing.ZeroHash)
 	} else {
-		return gitLog(dirs, fmt.Sprintf("tags/%s..HEAD", tag))
+		repo, err := git.PlainOpen(".")
+		if err != nil {
+			return nil, err
+		}
+
+		tagRef, err := repo.Tag(tag)
+		if err != nil {
+			return nil, err
+		}
+
+		// Resolve the tag to a commit hash
+		var tagHash plumbing.Hash
+		switch tagRef.Hash().Type() {
+		case plumbing.CommitObject:
+			tagHash = tagRef.Hash()
+		case plumbing.TagObject:
+			tagObj, err := repo.TagObject(tagRef.Hash())
+			if err != nil {
+				return nil, err
+			}
+			tagHash = tagObj.Target
+		default:
+			return nil, fmt.Errorf("unsupported tag type: %s", tagRef.Hash().Type())
+		}
+
+		return gitLog(dirs, tagHash)
 	}
 }
 
-func run(args ...string) (string, error) {
-	extraArgs := []string{
-		"-c", "log.showSignature=false",
-	}
-	args = append(extraArgs, args...)
-	/* #nosec */
-	cmd := exec.Command("git", args...)
-	bts, err := cmd.CombinedOutput()
-	if err != nil {
-		return "", errors.New(string(bts))
-	}
-	return string(bts), nil
-}
-
-func gitLog(dirs []string, refs ...string) ([]Commit, error) {
-	args := []string{"log", "--no-decorate", "--no-color", `--format=%H:%B<svu-commit-end>`}
-	args = append(args, refs...)
-	if len(dirs) > 0 {
-		args = append(args, "--")
-		args = append(args, dirs...)
-	}
-	s, err := run(args...)
+func gitLog(dirs []string, since plumbing.Hash) ([]Commit, error) {
+	repo, err := git.PlainOpen(".")
 	if err != nil {
 		return nil, err
 	}
+
+	var commitIter object.CommitIter
+	headRef, err := repo.Head()
+	if err != nil {
+		return nil, err
+	}
+
+	if since == plumbing.ZeroHash {
+		commitIter, err = repo.Log(&git.LogOptions{From: headRef.Hash()})
+	} else {
+		commitIter, err = repo.Log(&git.LogOptions{
+			From:  headRef.Hash(),
+			Since: &since,
+		})
+	}
+	if err != nil {
+		return nil, err
+	}
+	defer commitIter.Close()
+
 	var result []Commit
-	for _, commit := range strings.Split(s, "<svu-commit-end>") {
-		commit = strings.TrimSpace(commit)
-		if commit == "" { // accounts for the last split, which will be an empty line
-			continue
+	for {
+		commit, err := commitIter.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
 		}
 
-		hashEndIdx := strings.Index(commit, ":")
-		titleEndIdx := strings.Index(commit, "\n")
+		// Skip if we've reached the since commit
+		if since != plumbing.ZeroHash && commit.Hash == since {
+			break
+		}
+
+		// Filter by directories if specified
+		if len(dirs) > 0 {
+			files, err := commit.Files()
+			if err != nil {
+				return nil, err
+			}
+
+			var found bool
+			err = files.ForEach(func(file *object.File) error {
+				for _, dir := range dirs {
+					if strings.HasPrefix(file.Name, dir) {
+						found = true
+						return io.EOF // break early
+					}
+				}
+				return nil
+			})
+			if err != nil && err != io.EOF {
+				return nil, err
+			}
+			if !found {
+				continue
+			}
+		}
+
+		message := commit.Message
+		titleEndIdx := strings.Index(message, "\n")
+		var title, body string
 		if titleEndIdx < 0 {
-			titleEndIdx = len(commit)
+			title = message
+			body = ""
+		} else {
+			title = message[:titleEndIdx]
+			body = message[titleEndIdx+1:]
 		}
 
 		result = append(result, Commit{
-			commit[:hashEndIdx],
-			commit[hashEndIdx+1 : titleEndIdx],
-			commit[min(titleEndIdx+1, len(commit)):],
+			commit.Hash.String(),
+			title,
+			body,
 		})
 	}
+
 	return result, nil
 }
